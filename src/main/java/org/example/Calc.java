6cc526b34456607a0603e5a8042db8678845cbec
Yullc <apanan0910@gmail.com>
2025-03-19 14:56:38 +0900

    03.19 ((10+20)) == 30 강사님 풀이

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,64 +1,46 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Calc {
 
     public static int run(String exp) {
-
-        boolean needToMulti = exp.contains("*");
-        boolean needToPlus = exp.contains("+");
-        boolean needTole = exp.contains("(");
+        // 괄호 제거
+        exp = stripOuterBrackets(exp);
+        // 단일항이 들어오면 바로 리턴
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
+        }
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
 
         boolean needToCompound = needToPlus && needToMulti;
 
-        if (needToCompound&&needTole) {
-            int a;
-            int b = 0;
-
-            exp = exp.replace("(", "");
-            exp = exp.replace(")", "");
-            String[] bits = exp.split(" \\* ");
-            for (int i = 0; i < bits.length; i++) {
-
-                if (bits[i].contains("+")) {
-                    String[] arr = bits[i].split(" \\+ ");
-                    for (int j = 0; j < arr.length; j++) {
-
-                        b += Integer.parseInt(arr[j]);
-
-                    }
-
-                } else {
-                    a = run(bits[i]);
-                    System.out.println("a=" + a);
-                    b *= a;
-                }
-                System.out.println("sum=" + b);
-            }
-            return b;
-        }
-        else if (needToCompound) {
-            int a;
-            int b;
-            int sum = 0;
+        if (needToCompound) {
             String[] bits = exp.split(" \\+ ");
-            for (int i = 0; i < bits.length; i++) {
-                if (bits[i].contains("*")) {
-                    a = run(bits[i]);
-                    System.out.println("a=" + a);
-                    sum += a;
-                } else {
-                    b = Integer.parseInt(bits[i]);
-                    System.out.println("b=" + b);
-                    sum += b;
-                }
-                System.out.println("sum=" + sum);
-            }
-            return sum;
-        }
-        if (needToMulti) {
 
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        }
+
+        if (needToPlus) {
+            exp = exp.replace("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
             String[] bits = exp.split(" \\* ");
 
             int sum = 1;
@@ -66,31 +48,20 @@ public class Calc {
             for (int i = 0; i < bits.length; i++) {
                 sum *= Integer.parseInt(bits[i]);
             }
-            return sum;
-        } else {
-            exp = exp.replace("- ", "+ -");
-            if (needTole) {
-                exp = exp.replace("(", "");
-                exp = exp.replace(")", "");
 
-                String[] bits = exp.split(" \\+ ");
-                int sum = 0;
-                for (int i = 0; i < bits.length; i++) {
-                    sum += Integer.parseInt(bits[i]);
-                    System.out.println("sum=" + sum);
-                }
-                return sum;
-            }
-            else {
-            String[] bits = exp.split(" \\+ ");
-            int sum = 0;
-            for (int i = 0; i < bits.length; i++) {
-                sum += Integer.parseInt(bits[i]);
-            }
             return sum;
-            }
-
         }
 
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
     }
+
+    private static String stripOuterBrackets(String exp) {
+        if (exp.contains("(")&&exp.contains(")")) {
+            exp = exp.replace("(", "");
+            exp = exp.replace(")", "");
+        }
+
+        return exp;
+    }
+
 }
